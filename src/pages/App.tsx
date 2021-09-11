131c215159d6434f6b4392aacae8d5952c5c0063
Ivan <miltonivan117@gmail.com>
2021-09-10 18:03:21 -0600

    env delete

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,9 @@
 import { FC } from 'react'
 
 const TheApp:FC = () => {
-// const firebase = useFirebaseApp()
-// console.log(firebase)
-
-
     return (
         <div>
             <h1>Student App</h1>
-            {/* <button onClick={}>Login</button> */}
         </div>
     )
 }
